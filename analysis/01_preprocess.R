d468b4b981325978130da0c5ee3bfa91945d6f6a
Yiqing Li <yiqing.li.neuro@gmail.com>
2025-12-20 01:50:53 +0800

    Add analysis skeleton scripts
    Add initial preprocessing script for DEACMP sMRI study

---
+++ analysis/01_preprocess.R
@@ -0,0 +1,6 @@
+# 01_preprocess.R
+# Preprocessing pipeline for the DEACMP sMRI study
+# NOTE: Raw MRI data are not included in this repository.
+
+# TODO: define paths / parameters
+# TODO: preprocessing steps
