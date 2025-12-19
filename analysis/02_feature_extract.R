f61d0138cc9b8cd8d874fb61e442d43134f1adaf
Yiqing Li <yiqing.li.neuro@gmail.com>
2025-12-20 01:51:36 +0800

    analysis skeleton scripts
    Added a new script for feature extraction with TODOs.

---
+++ analysis/02_feature_extract.R
@@ -0,0 +1,6 @@
+# 02_feature_extract.R
+# Feature extraction to generate the analysis-ready feature matrix
+
+# TODO: load preprocessed outputs
+# TODO: extract morphometric / ROI features
+# TODO: save derived feature matrix (not stored in this repo)
